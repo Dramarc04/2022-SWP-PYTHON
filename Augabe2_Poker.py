a94d0a0014fb4d2418ae3615c9e0029ca6d437b1
Dramarc04 <dramarclp@gmail.com>
2022-11-09 09:03:32 +0100

    Statistik zu Aufgabe2_Poker.py hinzugefügt. Two Pair und FullHouse immernoch nicht vorhanden Komische Ergebnisse bei Statistik (Wenigr High Card als Paare)

--- Augabe2_Poker.py
+++ Augabe2_Poker.py
@@ -28,15 +28,14 @@ def initialiseDeck():
             c = c + 1
     return cardDeck
 
-def give_hand(deck):
+def give_hand(deck, size):
     hand = []
-    for i in range(5):
-        x = random.randrange(1,len(deck))
+    for i in range(size):
+        x = random.randrange(1,len(deck)+1)
         while not deck[x].in_deck:
-            x = random.randrange(1, 53)
+            x = random.randrange(1, len(deck)+1)
         hand.append(deck[x])
     reset_deck(deck)
-
     return hand
 
 def detect_ptq(hand):
@@ -63,8 +62,8 @@ def detect_street(hand):
         for card in hand:
             numbers.append(card.count)
         if not numbers.__contains__(lowest_value+i):
-            return 0
-    return 1
+            return False
+    return True
 
 def detect_flush(hand):
     instance = 0
@@ -73,8 +72,8 @@ def detect_flush(hand):
         if card.color == scard.color:
             instance = instance +1
     if instance == 5:
-        return 1
-    return 0
+        return True
+    return False
 
 #def detect_fullhouse(hand):
 #TBD
@@ -98,8 +97,8 @@ def detect_sflush(hand):
         if card.color == scard.color:
             instance = instance + 1
     if instance == 5 and street_true == True:
-        return 1
-    return 0
+        return True
+    return False
 
 def detect_rflush(hand):
     card = hand[0]
@@ -119,25 +118,75 @@ def detect_rflush(hand):
         if card.color == scard.color:
             instance = instance + 1
     if instance == 5 and street_true == True:
-        return 1
-    return 0
+        return True
+    return False
 
 def reset_deck(deck):
-    for i in range(len(deck)):
+    for i in range(1, len(deck)+1):
         deck[i].in_deck = True
+
+def check_hand(hand):
+    if(detect_rflush(hand) == True):
+        return 1
+    if(detect_sflush(hand) == True):
+        return 2
+    if(detect_ptq(hand) == 4):
+        return 3
+    #detect full house
+    if(detect_flush(hand) == True):
+        return 5
+    if(detect_street(hand) == True):
+        return 6
+    if(detect_ptq(hand) == 3):
+        return 7
+    #detect two pair
+    if(detect_ptq(hand) == 2):
+        return 9
+    return 10
+
+def check_times(attempts, deck):
+    statistic = {
+        "HCard": 0,
+        "Pair":0,
+        "TPair": 0,
+        "Triple": 0,
+        "Street": 0,
+        "Flush": 0,
+        "FullHouse": 0,
+        "Quadruple": 0,
+        "SFlush": 0,
+        "RFlush": 0
+    }
+    for i in range(attempts):
+        hand = give_hand(deck,5)
+        result = check_hand(hand)
+        if result == 1:
+            statistic["RFlush"] += 1
+        if result == 2:
+            statistic["SFlush"] += 1
+        if result == 3:
+            statistic["Quadruple"] += 1
+        if result == 4:
+            statistic["FullHouse"] += 1
+        if result == 5:
+            statistic["Flush"] += 1
+        if result == 6:
+            statistic["Street"] += 2
+        if result == 7:
+            statistic["Triple"] += 1
+        if result == 8:
+            statistic["TPair"] += 1
+        if result == 9:
+            statistic["Pair"] += 1
+        if result == 10:
+            statistic["HCard"] += 1
+    return statistic
+
 
 if __name__ == '__main__':
     deck = initialiseDeck()
     for i in deck:
         print(deck[i])
-    print(deck[2].in_deck)
-    hand = give_hand(deck)
-    for cards in hand:
-        print(str(cards))
-    output = detect_ptq(hand)
-    print(output)
-
-    test_hand = [card(1,1),card(1,10),card(1,11),card(1,12),card(1,13)]
-    output = detect_rflush(test_hand)
+    output = check_times(100000,deck)
     print(output)
 
